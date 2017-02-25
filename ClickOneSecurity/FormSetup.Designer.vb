c4b006ef712da78258eafd80564405078b3e53e1
ssdutnowind <nowind@vip.qq.com>
2017-02-25 13:11:44 +0800

    增加安装部署辅助工具

---
+++ ClickOneSecurity/FormSetup.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormSetup
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormSetup))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.BtnSetup = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BtnShowLog = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(329, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "本工具将在完成必要的检查后启动「协同编纂工具」的安装。"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 122)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(460, 127)
+        Me.TextBox1.TabIndex = 1
+        '
+        'BtnSetup
+        '
+        Me.BtnSetup.Location = New System.Drawing.Point(372, 76)
+        Me.BtnSetup.Name = "BtnSetup"
+        Me.BtnSetup.Size = New System.Drawing.Size(100, 23)
+        Me.BtnSetup.TabIndex = 2
+        Me.BtnSetup.Text = "开始检查并安装"
+        Me.BtnSetup.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 32)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(460, 23)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 12)
+        Me.Label2.TabIndex = 4
+        '
+        'BtnShowLog
+        '
+        Me.BtnShowLog.Location = New System.Drawing.Point(266, 76)
+        Me.BtnShowLog.Name = "BtnShowLog"
+        Me.BtnShowLog.Size = New System.Drawing.Size(100, 23)
+        Me.BtnShowLog.TabIndex = 5
+        Me.BtnShowLog.Text = "↓显示详细日志"
+        Me.BtnShowLog.UseVisualStyleBackColor = True
+        '
+        'FormSetup
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 111)
+        Me.Controls.Add(Me.BtnShowLog)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.BtnSetup)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FormSetup"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "协同编纂工具安装助手"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents BtnSetup As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Label2 As Label
+    Friend WithEvents BtnShowLog As Button
+End Class
---
+++ ClickOneSecurity/FormSetup.vb
@@ -0,0 +1,127 @@
+﻿Public Class FormSetup
+    Private Sub BtnSetup_Click(sender As Object, e As EventArgs) Handles BtnSetup.Click
+        Me.ProgressBar1.Value = 20
+        AddLog("开始「协同编纂工具」的安装准备！")
+        ' 优化安全策略
+        Me.ProgressBar1.Value = 40
+        AddLog("正在优化系统安全策略……")
+        CheckSystemSecurity()
+        ' 注册表注册
+        Me.ProgressBar1.Value = 60
+        AddLog("正在注册自定义URL协议……")
+        RegisterCustomPortol()
+        Me.ProgressBar1.Value = 80
+        AddLog("完成安装前的准备！")
+        StartSetup()
+        Me.ProgressBar1.Value = 100
+        AddLog("尝试启动Microsoft ClickOne！")
+        AddLog("请在完成安装后关闭当前窗口！")
+
+    End Sub
+
+    ''' <summary>
+    ''' 记录日志
+    ''' </summary>
+    ''' <param name="msg"></param>
+    Private Sub AddLog(msg)
+        Me.TextBox1.Text += msg + vbCrLf + vbCrLf
+        Me.Label2.Text = msg
+    End Sub
+
+    ''' <summary>
+    ''' 调整优化系统安全策略
+    ''' </summary>
+    Private Sub CheckSystemSecurity()
+        Try
+            ' 32位
+            Dim key As Microsoft.Win32.RegistryKey
+            key = Microsoft.Win32.Registry.LocalMachine.CreateSubKey("SOFTWARE\MICROSOFT\.NETFramework\Security\TrustManager\PromptingLevel")
+            key.SetValue("MyComputer", "Enabled")
+            key.SetValue("LocalIntranet", "Enabled")
+            key.SetValue("Internet", "Enabled")
+            key.SetValue("TrustedSites", "Enabled")
+            key.SetValue("UntrustedSites", "Enabled")
+            key.Close()
+        Catch ex As Exception
+            Me.TextBox1.Text += ex.Message
+        End Try
+
+        Try
+            ' 64位
+            Dim key As Microsoft.Win32.RegistryKey
+            key = Microsoft.Win32.Registry.LocalMachine.CreateSubKey("SOFTWARE\Wow6432Node\MICROSOFT\.NETFramework\Security\TrustManager\PromptingLevel")
+            key.SetValue("MyComputer", "Enabled")
+            key.SetValue("LocalIntranet", "Enabled")
+            key.SetValue("Internet", "Enabled")
+            key.SetValue("TrustedSites", "Enabled")
+            key.SetValue("UntrustedSites", "Enabled")
+            key.Close()
+        Catch ex As Exception
+            Me.TextBox1.Text += ex.Message
+        End Try
+    End Sub
+
+    ''' <summary>
+    ''' 注册自定义协议
+    ''' </summary>
+    Private Sub RegisterCustomPortol()
+        '' 当前程序路径
+        'Dim assemblyInfo = System.Reflection.Assembly.GetExecutingAssembly()
+        'Dim path = My.Computer.FileSystem.GetParentPath(assemblyInfo.Location)
+        'path += "\winword.reg"
+
+        'Dim regPath = """" + path + """"
+
+        'Try
+        '    Process.Start("regedit", String.Format(" /s {0}", regPath))
+        'Catch ex As Exception
+        '    Me.TextBox1.Text += ex.Message
+        'End Try
+
+        ' [HKEY_CLASSES_ROOT\MSWORD]
+        ' @="URL:MSWORD Protocol Handler"
+        ' "URL Protocol"=""
+        Try
+            Dim key As Microsoft.Win32.RegistryKey
+            key = Microsoft.Win32.Registry.ClassesRoot.CreateSubKey("MSWORD")
+            key.SetValue("", "URL:MSWORD Protocol Handler")
+            key.SetValue("URL Protocol", "")
+            ' [HKEY_CLASSES_ROOT\MSWORD\shell]
+            key = key.CreateSubKey("shell")
+            ' [HKEY_CLASSES_ROOT\MSWORD\shell\open]
+            key = key.CreateSubKey("open")
+            ' [HKEY_CLASSES_ROOT\MSWORD\shell\open\command]
+            ' @="WINWORD.exe /n /\"%1\""
+            key = key.CreateSubKey("command")
+            key.SetValue("", "WINWORD.exe /n /""%1""")
+        Catch ex As Exception
+            Me.TextBox1.Text += ex.Message
+        End Try
+
+    End Sub
+
+    ''' <summary>
+    ''' 开始安装
+    ''' </summary>
+    Private Sub StartSetup()
+        ' 当前程序路径
+        Dim assemblyInfo = System.Reflection.Assembly.GetExecutingAssembly()
+        Dim path = My.Computer.FileSystem.GetParentPath(assemblyInfo.Location)
+        path += "\setup.exe"
+
+        Try
+            Process.Start(path)
+        Catch ex As Exception
+            Me.TextBox1.Text += ex.Message
+        End Try
+    End Sub
+
+    ''' <summary>
+    ''' 显示详细日志
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub BtnShowLog_Click(sender As Object, e As EventArgs) Handles BtnShowLog.Click
+        Me.Height = 300
+    End Sub
+End Class
