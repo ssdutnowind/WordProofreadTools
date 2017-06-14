9b3c6eec5f2c524a42bcec66c78d1d59a1144ec6
Nowind <nowind@vip.qq.com>
2017-06-14 17:44:17 +0800

    最新版本~

---
+++ WordProofreadTools/FormStandard4.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormStandard4
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.TabPage4 = New System.Windows.Forms.TabPage()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.ColumnName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColumnUnit = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColumnSymbol = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColumnAction = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Controls.Add(Me.TabPage4)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(560, 277)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.DataGridView1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(552, 251)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "基本单位"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(552, 251)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "导出单位"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(552, 251)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "数单位词头"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'TabPage4
+        '
+        Me.TabPage4.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage4.Size = New System.Drawing.Size(552, 251)
+        Me.TabPage4.TabIndex = 3
+        Me.TabPage4.Text = "法定计量单位"
+        Me.TabPage4.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ColumnName, Me.ColumnUnit, Me.ColumnSymbol, Me.ColumnAction})
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 3)
+        Me.DataGridView1.MultiSelect = False
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersVisible = False
+        Me.DataGridView1.RowTemplate.Height = 23
+        Me.DataGridView1.Size = New System.Drawing.Size(546, 245)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'ColumnName
+        '
+        Me.ColumnName.HeaderText = "量的名称"
+        Me.ColumnName.Name = "ColumnName"
+        Me.ColumnName.ReadOnly = True
+        '
+        'ColumnUnit
+        '
+        Me.ColumnUnit.HeaderText = "单位名称"
+        Me.ColumnUnit.Name = "ColumnUnit"
+        Me.ColumnUnit.ReadOnly = True
+        '
+        'ColumnSymbol
+        '
+        Me.ColumnSymbol.HeaderText = "单位符号"
+        Me.ColumnSymbol.Name = "ColumnSymbol"
+        Me.ColumnSymbol.ReadOnly = True
+        '
+        'ColumnAction
+        '
+        Me.ColumnAction.HeaderText = "操作"
+        Me.ColumnAction.Name = "ColumnAction"
+        Me.ColumnAction.ReadOnly = True
+        '
+        'FormStandard4
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(584, 361)
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FormStandard4"
+        Me.Text = "国际单位制"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As Windows.Forms.TabPage
+    Friend WithEvents DataGridView1 As Windows.Forms.DataGridView
+    Friend WithEvents ColumnName As Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents ColumnUnit As Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents ColumnSymbol As Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents ColumnAction As Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents TabPage3 As Windows.Forms.TabPage
+    Friend WithEvents TabPage4 As Windows.Forms.TabPage
+End Class
--- WordProofreadTools/Ribbon.vb
+++ WordProofreadTools/Ribbon.vb
@@ -676,6 +676,33 @@ Public Class Ribbon
 
 #End Region
 
+#Region "排版"
+    ''' <summary>
+    ''' 纸张大小
+    ''' </summary>
+    ''' <param name="control"></param>
+    Public Sub BtnSetPaperSize_Click(ByVal control As Office.IRibbonControl)
+        CommonModule.Log("[Ribbon] 设置纸张大小：" + control.Tag)
+        Dim charVar As Char = "*"
+        Dim app = Globals.ThisAddIn.Application
+        Dim size = control.Tag.Split(charVar)
+        Dim width = app.CentimetersToPoints(Double.Parse(size(0)))
+        Dim height = app.CentimetersToPoints(Double.Parse(size(1)))
+        'Dim width = Single.Parse(size(0))
+        'Dim height = Single.Parse(size(1))
+        'app.ActiveDocument.PageSetup.PaperSize = WdPaperSize.wdPaperCustom
+        For Each sec As Section In app.ActiveDocument.Sections
+            sec.PageSetup.PaperSize = WdPaperSize.wdPaperCustom
+            sec.PageSetup.PageWidth = width
+            sec.PageSetup.PageHeight = height
+        Next
+        'app.ActiveDocument.PageSetup.PageWidth = width
+        'app.ActiveDocument.PageSetup.PageHeight = height
+
+    End Sub
+#End Region
+
+
 #Region "事件"
 
     'Public Sub OnUserLogin(ByVal sender As Object) Handles loginForm.UserLogin
